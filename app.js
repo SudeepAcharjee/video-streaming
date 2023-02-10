078dd791cd53132bd598d6d5680b9ec4d29cbc37
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2023-02-10 14:42:48 +0530

    Codes

---
+++ app.js
@@ -0,0 +1,8 @@
+const video = document.getElementById("video");
+
+// Add a listener for the timeupdate event to update the video progress bar
+video.addEventListener("timeupdate", updateProgressBar, false);
+
+function updateProgressBar() {
+  // Your code here
+}
